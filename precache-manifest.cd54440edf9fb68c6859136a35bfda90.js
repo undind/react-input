decdfba1fcfcd8d3de379cc4373cd27e44697036
undind <flyker777@yandex.ru>
2021-01-01 19:54:36 +0300

    Updates

---
+++ precache-manifest.cd54440edf9fb68c6859136a35bfda90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32049dbdddde6b64ffeb049a8fe408de",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d64b718e580a9ff6d129",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "6eb51b6f5fe1b431b75c",
+    "url": "./static/js/2.af2c3e2e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.af2c3e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64b718e580a9ff6d129",
+    "url": "./static/js/main.aaa6f9c3.chunk.js"
+  },
+  {
+    "revision": "06ec97225b08cb8237b0",
+    "url": "./static/js/runtime-main.30eb2eb1.js"
+  }
+]);
